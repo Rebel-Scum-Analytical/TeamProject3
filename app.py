1c64dc371f67ec513122e17027a536a5e157e7ab
Pratima Gokhale <pratima.gokhale@gmail.com>
2020-05-22 01:47:30 -0500

    trying to display the result of the queued task

--- app.py
+++ app.py
@@ -1091,7 +1091,7 @@ def analysis():
             output = get_status(job)
             data_to_display = pd.DataFrame(columns=["Message"],data=[ "Processing the and fetching the recommended food"])
             tables = [data_to_display.to_html(classes='table table-dark', table_id ='diary-table', justify='center')]
-            return jsonify({}), 202, {'Location': url_for('app.job_status', job_id=job.get_id())}
+            
             
 
             # basket_NDB = hillClimbing(deficient_nutrients,displaylist, target_nutrients_corrected, 5)
@@ -1112,25 +1112,9 @@ def analysis():
     
         return render_template("food_reco.html", tables=tables)
 
-            
-
-
     return render_template("Daily_vizualization.html")
 
-@bp.route('/status/<job_id>')
-def job_status(job_id):
-    q = Queue()
-    job = q.fetch_job(job_id)
-    if job is None:
-        response = {'status': 'unknown'}
-    else:
-        response = {
-            'status': job.get_status(),
-            'result': job.result,
-        }
-        if job.is_failed:
-            response['message'] = job.exc_info.strip().split('\n')[-1]
-    return jsonify(response)
+
 
 # Function to check if the user is logged in and maintain the infomration in session variable.
 # This is used in multiple routes.
